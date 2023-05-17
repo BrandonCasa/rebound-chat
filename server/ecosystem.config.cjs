dfbb0bbc7fbee8963a150247626480de7bbee8a8
Brandon Casamichana <brandoncasa8@gmail.com>
2023-05-16 21:47:57 -0400

    added auto restart delay

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
       watch_delay: 1000,
       ignore_watch: ['node_modules', 'public', 'mongod'],
       exp_backoff_restart_delay: 100,
+      restart_delay: 5000,
     },
   ],
 };
