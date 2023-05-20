8d1855fa7e7bf6be9b7f104b43a505e553993eaa
Brandon Casamichana <brandoncasa8@gmail.com>
2023-05-20 06:15:50 -0400

    rebuilding server using jwt

--- server/src/index.js
+++ server/src/index.js
@@ -1,138 +1,98 @@
-// Imports
-import express from 'express';
-import dotenv from 'dotenv';
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import path from 'path';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import multer from 'multer';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import { fileURLToPath } from 'url';
-import winston from 'winston';
-import passport from 'passport';
-import session from 'express-session';
-import LocalStrategy from 'passport-local';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.js';
-import userRoutes from './routes/user.js';
-import { register } from './controllers/auth.js';
-import { User } from './models/User.js';
+// Required packages
+const express = require('express');
+const multer = require('multer');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const winston = require('winston');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const dotenv = require('dotenv');
 
 // Load environment variables
 dotenv.config();
 
-// Configurations
-const fileName = fileURLToPath(import.meta.url);
-const dirName = path.dirname(fileName);
+// Start new MongoMemoryServer only in development environment
+let mongoServer;
+if (process.env.NODE_ENV === 'development') {
+  mongoServer = new MongoMemoryServer();
+}
+
+// Setup winston logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+// Use Multer for file handling
+const upload = multer({ dest: 'uploads/' });
+
+// Initialize express
 const app = express();
 
-// Logger setup
-const logger = winston.createLogger({
-  level: 'error',
-  transports: [new winston.transports.Console()],
-});
-
-// Error logging middleware
-app.use((err, req, res, next) => {
-  logger.error(err.stack);
-  next(err);
-});
-
-// Middleware setup
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(morgan('common'));
-app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
-app.use(cookieParser(process.env.SECRET));
-app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SECRET,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-// File storage setup
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'storage/');
-  },
-  filename(req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage });
-
-// Passport setup
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Routes
-// app.post('/api/auth/register', upload.single('picture'), register);
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-
-// Initialize the development environment
-async function initDev() {
-  if (process.env.NODE_ENV === 'development') {
-    const mongodbDev = await MongoMemoryServer.create({
-      instance: {
-        port: 27017,
-        dbName: 'rebound',
-        dbPath: './mongod/',
-        auth: false,
-      },
-      binary: {
-        version: '6.0.4',
-      },
-    });
-    await mongodbDev.stop();
-    await mongodbDev.start(true);
-    return mongodbDev;
-  } else {
-    return null;
-  }
-}
-
-// Start the server
-async function startServer() {
-  const mongodbDev = await initDev();
-
-  // Mongoose setup
-  const PORT = process.env.PORT || 6001;
-  let connectString = 'mongodb://localhost:27017/';
-  let extraParams = {};
-  if (process.env.NODE_ENV === 'development') {
-    connectString = mongodbDev.getUri();
-    console.log('Connection String:', connectString);
-  } else {
-    extraParams = {
-      user: process.env.DB_USER,
-      pass: process.env.DB_PASS,
-      authSource: 'admin',
-    };
-  }
-  await mongoose.set('strictQuery', true);
-  await mongoose.connect(`${connectString}rebound?w=majority`, {
-    retryWrites: true,
-    retryReads: true,
+// Connect to MongoDB
+(async () => {
+  let mongoUri;
+  const mongooseOpts = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    ...extraParams,
+  };
+
+  if (process.env.NODE_ENV === 'development') {
+    // Use in-memory mongoDB
+    await mongoServer.start()
+    mongoUri = await mongoServer.getUri();
+  } else {
+    // Use local mongoDB server
+    mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH}`;
+  }
+
+  mongoose.connect(mongoUri, mongooseOpts);
+
+  mongoose.connection.on('error', (e) => {
+    if (e.message.code === 'ETIMEDOUT') {
+      logger.error(e);
+      mongoose.connect(mongoUri, mongooseOpts);
+    }
+    logger.error(e);
   });
 
-  // Start listening for incoming requests
-  app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  mongoose.connection.once('open', () => {
+    logger.info(`MongoDB successfully connected to ${mongoUri}`);
   });
-}
+})();
 
-// Call the startServer function to initialize the server
-startServer();
+// Middleware for body parsing
+app.use(express.json());
+
+// Apply helmet middleware to set secure HTTP headers
+app.use(helmet());
+
+// Apply Morgan middleware for logging HTTP requests
+app.use(morgan('combined', { stream: winston.stream }));
+
+// Create a test endpoint for file upload
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+});
+
+// Test endpoint for JWT
+app.get('/jwt', (req, res) => {
+  const token = jwt.sign({ user: 'username' }, 'your_secret_key', { expiresIn: '1h' });
+  res.json({ token: token });
+});
+
+// Start server
+app.listen(process.env.PORT, () => logger.info(`Server started on port ${process.env.PORT}`));
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  if (process.env.NODE_ENV === 'development') {
+    await mongoServer.stop();
+  }
+  process.exit();
+});
--- server/src/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const verifyToken = async (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  return res.redirect(301, '/api/auth/login');
-};
-
-export default verifyToken;
