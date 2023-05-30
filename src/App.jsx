c2dd21721d39aa44acbe59948810ed0ee4f778c0
Brandon Casamichana <brandoncasa8@gmail.com>
2023-05-30 08:33:12 -0400

    changes

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./routes/LandingPage.route";
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CustomAppBar from "./components/CustomAppBar.comp";
+import darkTheme from "./helpers/darkTheme";
+import RegisterDialog from "./components/RegisterDialog.comp";
+import LoginDialog from "./components/LoginDialog.comp";
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoggedIn } from "./reducers/authReducer";
+import axios from "axios";
+
+function App() {
+  const authTokenState = useSelector((state) => state.auth.authToken);
+  const loggedInState = useSelector((state) => state.auth.loggedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (authTokenState && authTokenState !== "" && !loggedInState) {
+      // verify at /api/auth/verify
+      const requestString = process.env.NODE_ENV === "development" ? "http://localhost:6001/api/auth/verify/" : "/api/auth/verify/";
+      axios
+        .get(requestString, {
+          headers: {
+            "Content-Type": "application/json",
+            "Allow-Control-Allow-Origin": "*",
+            Authorization: authTokenState,
+          },
+        })
+        .then((response) => {
+          dispatch(setLoggedIn({ loggedIn: true }));
+        })
+        .catch((error) => {
+          console.log(error);
+          window.localStorage.removeItem("auth-token");
+        });
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <RegisterDialog />
+      <LoginDialog />
+      <CustomAppBar>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </CustomAppBar>
+    </ThemeProvider>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { CssBaseline } from "@mui/material";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
