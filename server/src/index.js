9206ff4656d0cdba898331c86e33de6186840e0c
Brandon Casamichana <brandoncasawork8@gmail.com>
2023-06-16 11:21:51 -0400

    added rate limiting

--- server/src/index.js
+++ server/src/index.js
@@ -1,40 +1,47 @@
 // Required packages
-const express = require('express');
-const multer = require('multer');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
-const logger = require('./logging/logger');
-const cors = require('cors');
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const logger = require("./logging/logger");
+const cors = require("cors");
+const fs = require("fs");
+const rateLimit = require("express-rate-limit");
+
+const verifyLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Too many verification attempts from this IP, please try again after an hour",
+});
 
 // Require routes
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 // Load environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Start new MongoMemoryServer only in development environment
 
 let mongoServer;
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   if (!fs.existsSync("./dev")) {
     fs.mkdirSync("./dev");
   }
   mongoServer = new MongoMemoryServer({
     instance: {
       port: 27017,
-      dbPath: 'dev',
-    }
+      dbPath: "dev",
+    },
   });
 }
 
 // Use Multer for file handling
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 // Initialize express
 const app = express();
@@ -49,9 +56,9 @@ app.use(cors());
     useUnifiedTopology: true,
   };
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     // Use in-memory mongoDB
-    await mongoServer.start(true)
+    await mongoServer.start(true);
     mongoUri = await mongoServer.getUri();
   } else {
     // Use local mongoDB server
@@ -60,15 +67,15 @@ app.use(cors());
 
   mongoose.connect(mongoUri, mongooseOpts);
 
-  mongoose.connection.on('error', (e) => {
-    if (e.message.code === 'ETIMEDOUT') {
+  mongoose.connection.on("error", (e) => {
+    if (e.message.code === "ETIMEDOUT") {
       logger.error(e);
       mongoose.connect(mongoUri, mongooseOpts);
     }
     logger.error(e);
   });
 
-  mongoose.connection.once('open', () => {
+  mongoose.connection.once("open", () => {
     logger.info(`MongoDB successfully connected to ${mongoUri}`);
   });
 })();
@@ -88,7 +95,7 @@ logger.stream = {
 };
 
 // Then use morgan middleware with the stream of winston
-app.use(morgan('combined', { stream: logger.stream }));
+app.use(morgan("combined", { stream: logger.stream }));
 
 // Create a test endpoint for file upload
 //app.post('/api/upload', upload.single('file'), (req, res) => {
@@ -96,21 +103,20 @@ app.use(morgan('combined', { stream: logger.stream }));
 //});
 
 // Verify token endpoint
-app.get('/api/auth/verify', (req, res) => {
+app.get("/api/auth/verify", verifyLimiter, async (req, res) => {
   const valid = jwt.verify(req.headers.authorization, process.env.SECRET);
   res.send(valid);
 });
 
-
 // Middleware for routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Start server
 app.listen(process.env.PORT, () => logger.info(`Server started on port ${process.env.PORT}`));
 
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.connection.close();
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     await mongoServer.stop();
   }
   process.exit();
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,73 +1,89 @@
 // Required packages
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
-const { body, validationResult } = require('express-validator');
-const logger = require('../logging/logger');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
+const logger = require("../logging/logger");
+const rateLimit = require("express-rate-limit");
+
+const registerLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Too many accounts created from this IP, please try again after an hour",
+});
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Too many login attempts from this IP, please try again after an hour",
+});
 
 // Create express router
 const router = express.Router();
 
 // User model
-const User = mongoose.model('User', new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  displayName: { type: String, required: true },
-}));
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    displayName: { type: String, required: true },
+  })
+);
 
 // Validation rules
 const registerRules = [
   // Display name Rules
-  body('displayName').custom((value, { req }) => {
-    if (typeof value === 'string' && (value.toString().startsWith(' ') || value.toString().endsWith(' '))) {
-      throw new Error('Display name must not start or end with spaces.');
+  body("displayName").custom((value, { req }) => {
+    if (typeof value === "string" && (value.toString().startsWith(" ") || value.toString().endsWith(" "))) {
+      throw new Error("Display name must not start or end with spaces.");
     }
-    if (value === undefined || value === null || value === '') {
-      throw new Error('Display name is required.');
+    if (value === undefined || value === null || value === "") {
+      throw new Error("Display name is required.");
     }
     if (value.length < 3) {
-      throw new Error('Display name must be at least 3 characters long.');
+      throw new Error("Display name must be at least 3 characters long.");
     }
     if (value.length > 16) {
-      throw new Error('Display name cannot be longer than 16 characters.');
+      throw new Error("Display name cannot be longer than 16 characters.");
     }
     return true;
   }),
 
   // Username Rules
-  body('username').custom((value, { req }) => {
-    if (typeof value === 'string' && value.toString().includes(' ')) {
-      throw new Error('Username must not contain spaces.');
+  body("username").custom((value, { req }) => {
+    if (typeof value === "string" && value.toString().includes(" ")) {
+      throw new Error("Username must not contain spaces.");
     }
-    if (value === undefined || value === null || value === '') {
-      throw new Error('Username is required.');
+    if (value === undefined || value === null || value === "") {
+      throw new Error("Username is required.");
     }
     if (value.length < 3) {
-      throw new Error('Username must be at least 3 characters long.');
+      throw new Error("Username must be at least 3 characters long.");
     }
     if (value.length > 24) {
-      throw new Error('Username cannot be longer than 24 characters.');
+      throw new Error("Username cannot be longer than 24 characters.");
     }
     if (value !== value.toLowerCase()) {
-      throw new Error('Username must be lowercase.');
+      throw new Error("Username must be lowercase.");
     }
     return true;
   }),
 
   // Password Rules
-  body('password').custom((value, { req }) => {
-    if (typeof value === 'string' && value.toString().includes(' ')) {
-      throw new Error('Password must not contain spaces.');
+  body("password").custom((value, { req }) => {
+    if (typeof value === "string" && value.toString().includes(" ")) {
+      throw new Error("Password must not contain spaces.");
     }
-    if (value === undefined || value === null || value === '') {
-      throw new Error('Password is required.');
+    if (value === undefined || value === null || value === "") {
+      throw new Error("Password is required.");
     }
     if (value.length < 5) {
-      throw new Error('Password must be at least 5 characters long.');
+      throw new Error("Password must be at least 5 characters long.");
     }
     if (value.length > 50) {
-      throw new Error('Password cannot be longer than 50 characters.');
+      throw new Error("Password cannot be longer than 50 characters.");
     }
     return true;
   }),
@@ -80,13 +96,10 @@ const registerRules = [
   //}),
 ];
 
-const loginRules = [
-  body('username').exists().withMessage('Username is required'),
-  body('password').exists().withMessage('Password is required'),
-];
+const loginRules = [body("username").exists().withMessage("Username is required"), body("password").exists().withMessage("Password is required")];
 
 // Register endpoint
-router.post('/register', registerRules, async (req, res) => {
+router.post("/register", registerLimiter, registerRules, async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -97,7 +110,7 @@ router.post('/register', registerRules, async (req, res) => {
 
     // Check if user already exists
     let user = await User.findOne({ username: username });
-    if (user) return res.status(400).json({ errors: [{ msg: 'User already exists.' }] });
+    if (user) return res.status(400).json({ errors: [{ msg: "User already exists." }] });
 
     // Hash password
     const salt = await bcrypt.genSalt(10);
@@ -114,16 +127,16 @@ router.post('/register', registerRules, async (req, res) => {
     await user.save();
 
     // Create and assign a token
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: (stayLoggedIn ? '365d' : '1h') });
-    res.status(200).header('auth-token', token).send(token);
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: stayLoggedIn ? "365d" : "1h" });
+    res.status(200).header("auth-token", token).send(token);
   } catch (error) {
     logger.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // Login endpoint
-router.post('/login', loginRules, async (req, res) => {
+router.post("/login", loginLimiter, loginRules, async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -134,18 +147,18 @@ router.post('/login', loginRules, async (req, res) => {
 
     // Find user
     const user = await User.findOne({ username: username });
-    if (!user) return res.status(400).json({ errors: [{ msg: 'Invalid username or password.' }] });
+    if (!user) return res.status(400).json({ errors: [{ msg: "Invalid username or password." }] });
 
     // Validate password
     const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) return res.status(400).json({ errors: [{ msg: 'Invalid username or password.' }] });
+    if (!validPassword) return res.status(400).json({ errors: [{ msg: "Invalid username or password." }] });
 
     // Create and assign a token
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: (stayLoggedIn ? '365d' : '1h') });
-    res.header('auth-token', token).send(token);
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: stayLoggedIn ? "365d" : "1h" });
+    res.header("auth-token", token).send(token);
   } catch (error) {
     logger.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
