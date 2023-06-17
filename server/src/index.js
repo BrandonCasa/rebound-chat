26f9c1ae74a11dbd07cbf185a7a7a4cc568207f8
Brandon Casamichana <brandoncasawork8@gmail.com>
2023-06-17 08:29:37 -0400

    moved mongoserver out of index

--- server/src/index.js
+++ server/src/index.js
@@ -15,57 +15,18 @@ const logger = require("./logging/logger.js");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const MongoServer = require("./mongoose/mongoServer.js");
+let mongoServer = MongoServer.startServer();
+
 const verifyLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
   message: "Too many verification attempts from this IP, please try again after an hour",
 });
 
-let mongoServer;
-if (process.env.NODE_ENV === "development") {
-  if (!fs.existsSync("./dev")) {
-    fs.mkdirSync("./dev");
-  }
-  mongoServer = new MongoMemoryServer({
-    instance: {
-      port: 27017,
-      dbPath: "dev",
-    },
-  });
-}
-
 const app = express();
 
 app.use(cors());
-
-(async function () {
-  let mongoUri;
-  const mongooseOpts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
-
-  if (process.env.NODE_ENV === "development") {
-    await mongoServer.start(true);
-    mongoUri = await mongoServer.getUri();
-  } else {
-    mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH}`;
-  }
-
-  await mongoose.connect(mongoUri, mongooseOpts);
-
-  mongoose.connection.on("error", (e) => {
-    if (e.message.code === "ETIMEDOUT") {
-      logger.error(e);
-      mongoose.connect(mongoUri, mongooseOpts);
-    }
-    logger.error(e);
-  });
-
-  mongoose.connection.once("open", () => {
-    logger.info(`MongoDB successfully connected to ${mongoUri}`);
-  });
-})();
 
 app.use(express.json());
 
@@ -89,10 +50,7 @@ app.use("/api/auth", authRoutes);
 app.listen(process.env.PORT, () => logger.info(`Server started on port ${process.env.PORT}`));
 
 process.on("SIGINT", async () => {
-  await mongoose.connection.close();
-  if (process.env.NODE_ENV === "development") {
-    await mongoServer.stop();
-  }
+  MongoServer.stopServer();
   process.exit();
 });
 
