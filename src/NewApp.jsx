1a2e2b4b4aa05401afc2a6fc73648fb9032eb94f
Brandon Casamichana <brandoncasawork8@gmail.com>
2023-06-26 22:15:16 -0400

    fixed tailwind

--- src/NewApp.jsx
+++ src/NewApp.jsx
@@ -1,5 +1,11 @@
+import SideBar from "newcomponents/Sidebar_TW/sidebar";
+
 function NewApp() {
-  return <p className="text-red-500 text-center">xd</p>;
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  );
 }
 
 export default NewApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "false",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+      },
+    },
   },
   plugins: [],
 };
