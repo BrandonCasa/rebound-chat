93e6df772bfa4e447b06852b615f14222f8aacaf
Brandon Casamichana <brandoncasawork8@gmail.com>
2023-06-28 18:15:13 -0400

    removed craco

--- src/newcomponents/Sidebar_TW/sidebar.jsx
+++ src/newcomponents/Sidebar_TW/sidebar.jsx
@@ -1,6 +1,6 @@
 const SideBar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary   shadow-lg">
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow">
       <i>A</i>
       <i>B</i>
       <i>C</i>
