a56ca6fd929f2465a2fd550c10e6b8fda0229bb0
Brandon Casamichana <brandoncasa8@gmail.com>
2023-11-22 15:58:00 -0500

    rewrote indexjs

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import helmet from "helmet";
-import mongoose from "mongoose";
 import morgan from "morgan";
 import cors from "cors";
 import logger from "./logging/logger.js";
@@ -14,36 +13,68 @@ dotenv.config();
 class ServerBackend {
   constructor() {
     this.app = express();
+    this.initializeMiddleware();
     this.server = http.createServer(this.app);
-    this.app.use(cors());
+  }
+
+  initializeMiddleware() {
+    this.app.use(cors({ optionsSuccessStatus: 200 }));
     this.app.use(express.json());
     this.app.use(helmet());
 
-    logger.stream = {
-      write: function (message) {
-        logger.info(message);
+    this.configureLogger();
+  }
+
+  configureLogger() {
+    const morganOption = {
+      stream: {
+        write: (message) => logger.info(message.trim()),
       },
     };
 
-    this.app.use(morgan("combined", { stream: logger.stream }));
+    this.app.use(morgan("combined", morganOption));
   }
 
   async startBackend() {
-    await databaseServer.startServer();
-    this.server.listen(process.env.PORT, () => logger.info(`Server started on port ${process.env.PORT}`));
+    try {
+      await databaseServer.startServer();
+      const PORT = process.env.PORT || 3000; // Define a default port
+      this.server.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
+    } catch (error) {
+      logger.error("Failed to start the server:", error);
+    }
   }
 
   async stopBackend() {
-    await databaseServer.stopServer();
-    this.server.close();
+    try {
+      await databaseServer.stopServer();
+      this.server.close(() => logger.info("Server gracefully stopped"));
+    } catch (error) {
+      logger.error("Error while stopping the server:", error);
+      throw error;
+    }
   }
 }
 
-const serverBackend = new ServerBackend();
+// Initiate the server
+(async () => {
+  const serverBackend = new ServerBackend();
 
-process.on("SIGINT", async () => {
-  await serverBackend.stopBackend();
-  process.exit();
-});
+  // Ensure backend stops gracefully on SIGINT signal
+  process.on("SIGINT", async () => {
+    try {
+      await serverBackend.stopBackend();
+      process.exit(0);
+    } catch (e) {
+      logger.error("Failed to shut down the server:", e);
+      process.exit(1);
+    }
+  });
 
-serverBackend.startBackend();
+  try {
+    await serverBackend.startBackend();
+  } catch (e) {
+    logger.error("Startup error:", e);
+    process.exit(1);
+  }
+})();
