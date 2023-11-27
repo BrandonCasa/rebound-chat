b0087d363fb90e03074b00bd4c04a93b00b09fbe
Brandon Casamichana <brandoncasa8@gmail.com>
2023-11-27 15:27:57 -0500

    updated logging

--- server/src/logger.js
+++ server/src/logger.js
@@ -6,10 +6,20 @@ const logger = winston.createLogger({
   transports: [new winston.transports.File({ filename: "./logs/error.log", level: "error" }), new winston.transports.File({ filename: "./logs/combined.log" })],
 });
 
+let alignColorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.timestamp({
+    format: "(hh:mm:ss A) (MM-DD-YY)",
+  }),
+  winston.format.printf((info) => `[${info.level}] ${info.timestamp}: ${info.message}`)
+);
+
 if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: winston.format.combine(winston.format.colorize(), alignColorsAndTime),
     })
   );
 }
