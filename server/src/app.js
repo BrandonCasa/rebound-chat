85264536cbad091a6e70514834e4248016ad5055
BrandonCasa <brandoncasa8@gmail.com>
2024-10-03 15:06:07 -0400

    added error logging for socket backend

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import logger from "./logger.js";
 import { configDotenv } from "dotenv";
 import http from "http";
-import bodyParser from "body-parser";
 import methodOverride from "method-override";
 import session from "express-session";
 
@@ -17,72 +16,77 @@ import passport from "passport";
 configDotenv();
 
 class ServerBackend {
-  constructor() {
-    this.app = express();
-    customPassport.setupPassport();
-    this.initializeMiddleware();
-    this.initializeRoutes();
-    this.server = http.createServer(this.app);
-  }
+	constructor() {
+		this.app = express();
+		customPassport.setupPassport();
+		this.initializeMiddleware();
+		this.initializeRoutes();
+		this.server = http.createServer(this.app);
+	}
 
-  initializeMiddleware() {
-    this.app.use(cors({ optionsSuccessStatus: 200 }));
-    this.configureLogger();
+	initializeMiddleware() {
+		this.app.use(cors({ optionsSuccessStatus: 200 }));
+		this.configureLogger();
 
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-    this.app.use(bodyParser.json());
+		this.app.use(express.urlencoded({ extended: false }));
+		this.app.use(express.json());
 
-    this.app.use(methodOverride());
-  }
+		this.app.use(methodOverride());
+	}
 
-  configureLogger() {}
+	configureLogger() {}
 
-  initializeRoutes() {
-    this.app.use(routes);
-  }
+	initializeRoutes() {
+		this.app.use(routes);
+	}
 
-  async startBackend() {
-    try {
-      await databaseServer.startServer();
-      socketBackend.startListening();
-      const PORT = process.env.PORT || 6001; // Define a default port
-      this.server.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
-    } catch (error) {
-      logger.error("Failed to start the server:", error);
-    }
-  }
+	async startBackend() {
+		try {
+			await databaseServer.startServer();
+			try {
+				socketBackend.startListening();
+			} catch (socketError) {
+				logger.error("Failed to start socket backend:", socketError);
+				throw socketError;
+			}
+			const PORT = process.env.PORT || 6001; // Define a default port
+			this.server.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
+		} catch (error) {
+			logger.error("Failed to start the server:", error);
+		}
+	}
 
-  async stopBackend() {
-    try {
-      socketBackend.stopListening();
-      await databaseServer.stopServer();
-      this.server.close(() => logger.info("Server gracefully stopped"));
-    } catch (error) {
-      logger.error("Error while stopping the server:", error);
-      throw error;
-    }
-  }
+	async stopBackend() {
+		try {
+			socketBackend.stopListening();
+			await databaseServer.stopServer();
+			this.server.close(() => logger.info("Server gracefully stopped"));
+		} catch (error) {
+			logger.error("Error while stopping the server:", error);
+			throw error;
+		}
+	}
 }
 
 // Initiate the server
 (async () => {
-  const serverBackend = new ServerBackend();
+	const serverBackend = new ServerBackend();
 
-  // Ensure backend stops gracefully on SIGINT signal
-  process.on("SIGINT", async () => {
-    try {
-      await serverBackend.stopBackend();
-      process.exit(0);
-    } catch (e) {
-      logger.error("Failed to shut down the server:", e);
-      process.exit(1);
-    }
-  });
+	// Ensure backend stops gracefully on SIGINT signal
+	process.on("SIGINT", async () => {
+		try {
+			await serverBackend.stopBackend();
+			process.exit(0);
+		} catch (e) {
+			logger.error("Failed to shut down the server:", e);
+			process.exit(1);
+		}
+	});
 
-  try {
-    await serverBackend.startBackend();
-  } catch (e) {
-    logger.error("Startup error:", e);
-    process.exit(1);
-  }
+	try {
+		await serverBackend.startBackend();
+	} catch (e) {
+		logger.error("Startup error:", e);
+		process.exit(1);
+	}
 })();
