d8a201dfa94e43e54be10a9b4e81190fcef428db
BrandonCasa <brandoncasa8@gmail.com>
2024-10-21 10:56:11 -0400

    test snackbar

--- src/routes/TestingPage/TestingPage.route.jsx
+++ src/routes/TestingPage/TestingPage.route.jsx
@@ -1,5 +1,7 @@
 import { useTheme } from "@mui/material/styles";
-import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { addSnackbar } from "slices/snackbarSlice";
 
 function TestingCard({ title, description }) {
 	let theme = useTheme();
@@ -20,20 +22,26 @@ function TestingCard({ title, description }) {
 
 function TestingPage(props) {
 	const theme = useTheme();
+	const dispatch = useDispatch();
+
+	const warnSnackbar = () => {
+		dispatch(addSnackbar({ snackbarMsg: "This is a test snackbar.", snackbarSeverity: "warning", autoHideDuration: 2000 }));
+	};
 
 	return (
 		<Box sx={{ justifyContent: "center", flexGrow: 1, overflow: "hidden" }}>
 			<Grid container spacing={2} sx={{ justifyContent: "center" }}>
 				<Grid item xs={12} sm={4} md={4} justifyContent="center" sx={{ display: "flex" }}>
-					<Card sx={{ p: 0, flexGrow: 1 }}>
-						<CardContent>
-							<Typography variant="h5" sx={{ color: `${theme.palette.text.primary}` }}>
-								Test Alert
-							</Typography>
-							<Typography variant="body2" sx={{ color: `${theme.palette.text.secondary}` }}>
-								Spawn a 2 second error snackbar
-							</Typography>
-						</CardContent>
+					<Card sx={{ p: 2, flexGrow: 1 }}>
+						<Typography variant="h5" sx={{ color: `${theme.palette.text.primary}` }}>
+							Test Alert
+						</Typography>
+						<Typography variant="body2" sx={{ color: `${theme.palette.text.secondary}` }}>
+							Spawn a 2 second warning snackbar
+						</Typography>
+						<Button variant="contained" sx={{ mt: 1 }} color="warning" onClick={warnSnackbar}>
+							Alert
+						</Button>
 					</Card>
 				</Grid>
 			</Grid>
