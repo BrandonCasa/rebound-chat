d1aac4b0fba3b2c3c48f9b35ea46e953efdb06d8
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-02-03 09:27:11 -0500

    added setupListeners

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./slices";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 const store = configureStore({
-  reducer: rootReducer,
+	reducer: rootReducer,
 });
+
+setupListeners(store.dispatch);
 
 export default store;
