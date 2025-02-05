a939ba3b7f76428b05585fcd10db110e3410c1e0
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-02-05 14:50:48 -0500

    hopefully fixed electron

--- public/electron.js
+++ public/electron.js
@@ -4,22 +4,25 @@ const BrowserWindow = electron.BrowserWindow;
 
 const path = require("path");
 const url = require("url");
-const isDev = import("electron-is-dev");
 
 let mainWindow;
 
 function createWindow() {
 	mainWindow = new BrowserWindow({ width: 900, height: 680 });
-	mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+
+	mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "index.html")}`);
+
 	mainWindow.on("closed", () => (mainWindow = null));
+}
+
+function isDev() {
+	return !app.getAppPath().includes("app.asar");
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+	app.quit();
 });
 
 app.on("activate", () => {
@@ -27,5 +30,3 @@ app.on("activate", () => {
 		createWindow();
 	}
 });
-
-app.whenReady().then(createWindow);
