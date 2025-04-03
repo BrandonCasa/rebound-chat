6e9f57cccc1303c0f43fbb6640952a016d65ecfd
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-04-03 10:14:51 -0400

    yes

--- public/electron.js
+++ public/electron.js
@@ -1,37 +1,38 @@
-const { electron, dialog } = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const url = require("url");
 
 let mainWindow;
 
-function createWindow() {
-	mainWindow = new BrowserWindow({ width: 900, height: 680 });
+async function createWindow() {
+	const { default: isDev } = await import("electron-is-dev");
+
+	mainWindow = new BrowserWindow({
+		width: 900,
+		height: 680,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
 
 	if (isDev) {
 		mainWindow.loadURL("http://localhost:3000");
 	} else {
 		dialog.showErrorBox("HTML File Path", `file://${path.join(__dirname, "../build/index.html")}`);
-		mainWindow.loadFile(`file://${path.join(__dirname, "../build/index.html")}`);
+		mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
 	}
 
-	mainWindow.on("closed", () => (mainWindow = null));
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
 }
 
-function isDev() {
-	return app.isPackaged;
-}
-
-app.on("ready", createWindow);
+app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-	app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", () => {
-	if (mainWindow === null) {
-		createWindow();
-	}
+	if (mainWindow === null) createWindow();
 });
