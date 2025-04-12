d6a7a7ee9779ec3075c6d7e1914dfe8bfbcdb00c
BrandonCasa <brandoncasa8@gmail.com>
2025-04-12 13:07:20 -0400

    now using memory server replica set

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,80 +1,82 @@
 import fs from "fs";
 import logger from "../logger.js";
-import { MongoMemoryServer } from "mongodb-memory-server";
+import { MongoMemoryReplSet } from "mongodb-memory-server";
 import mongoose from "mongoose";
 
 class DatabaseServer {
-  constructor() {
-    this.mongoServer = null;
-  }
+	constructor() {
+		this.mongoServer = null;
+	}
 
-  async startServer() {
-    const isDevelopment = process.env.NODE_ENV === "development";
-    const mongoUri = isDevelopment ? await this.startDevelopmentServer() : this.getProductionUri();
+	async startServer() {
+		const isDevelopment = process.env.NODE_ENV === "development";
+		const mongoUri = isDevelopment ? await this.startDevelopmentServer() : this.getProductionUri();
 
-    const mongooseOpts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-    mongoose.set("strictQuery", false);
+		const mongooseOpts = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		};
+		mongoose.set("strictQuery", false);
 
-    await mongoose.connect(mongoUri, mongooseOpts, () => {});
-    this.setupEventListeners(mongoUri, mongooseOpts);
-  }
+		await mongoose.connect(mongoUri, mongooseOpts, () => {});
+		this.setupEventListeners(mongoUri, mongooseOpts);
+	}
 
-  async startDevelopmentServer() {
-    this.ensureDevDirectory();
-    this.mongoServer = new MongoMemoryServer({
-      instance: {
-        port: 27017,
-        dbPath: "./dev",
-        storageEngine: "wiredTiger",
-      },
-    });
-    await this.mongoServer.start();
-    const uri = await this.mongoServer.getUri();
-    logger.info(`In-memory MongoDB running at ${uri}`);
-    return uri;
-  }
+	async startDevelopmentServer() {
+		this.ensureDevDirectory();
+		this.mongoServer = new MongoMemoryReplSet({
+			instanceOpts: [
+				{
+					port: 27017,
+					dbPath: "./dev",
+					storageEngine: "wiredTiger",
+				},
+			],
+			replSet: { name: "rs0" },
+		});
+		await this.mongoServer.start();
+		const uri = await this.mongoServer.getUri();
+		logger.info(`In-memory MongoDB replica set running at ${uri}`);
+		return uri;
+	}
 
-  getProductionUri() {
-    return `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH}`;
-  }
+	getProductionUri() {
+		return `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH}&replicaSet=rs0`;
+	}
 
-  setupEventListeners(mongoUri, mongooseOpts) {
-    mongoose.connection.on("error", (e) => {
-      if (e.message.code === "ETIMEDOUT") {
-        logger.error(e);
-        mongoose.connect(mongoUri, mongooseOpts);
-      }
-      logger.error(e);
-    });
+	setupEventListeners(mongoUri, mongooseOpts) {
+		mongoose.connection.on("error", (e) => {
+			if (e.message.code === "ETIMEDOUT") {
+				logger.error(e);
+				mongoose.connect(mongoUri, mongooseOpts);
+			}
+			logger.error(e);
+		});
 
-    mongoose.connection.once("open", () => {
-      logger.info(`MongoDB successfully connected to ${mongoUri}`);
-    });
-  }
+		mongoose.connection.once("open", () => {
+			logger.info(`MongoDB successfully connected to ${mongoUri}`);
+		});
+	}
 
-  ensureDevDirectory() {
-    const devPath = "./dev";
-    if (!fs.existsSync(devPath)) {
-      fs.mkdirSync(devPath);
-    }
-  }
+	ensureDevDirectory() {
+		const devPath = "./dev";
+		if (!fs.existsSync(devPath)) {
+			fs.mkdirSync(devPath);
+		}
+	}
 
-  async stopServer() {
-    await mongoose.connection.close();
-    if (this.mongoServer) {
-      await this.mongoServer.stop();
-    }
-  }
+	async stopServer() {
+		await mongoose.connection.close();
+		if (this.mongoServer) {
+			await this.mongoServer.stop();
+		}
+	}
 
-  getMongoServer() {
-    return this.mongoServer;
-  }
+	getMongoServer() {
+		return this.mongoServer;
+	}
 }
 
 const databaseServer = new DatabaseServer();
 
-// Export an instantiated version to prevent recreating on import.
 export { databaseServer as default };
