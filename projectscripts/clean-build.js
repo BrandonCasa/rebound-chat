0f4a546943fa3ad0799a17963ff3b27f6e2a91aa
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-04-16 09:40:07 -0400

    fixed

--- projectscripts/clean-build.js
+++ projectscripts/clean-build.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+const packageJson = require("../package.json");
 
 const buildDir = path.join(__dirname, "..", "build");
+const appDir = path.join(__dirname, "..", "app");
 
 fs.readdir(buildDir, (err, files) => {
 	if (err) {
@@ -9,10 +11,32 @@ fs.readdir(buildDir, (err, files) => {
 		process.exit(1);
 	}
 
+	if (!fs.existsSync(appDir)) {
+		fs.mkdirSync(appDir);
+	} else {
+		// clear out app dir
+		fs.readdir(appDir, (err, filesApp) => {
+			filesApp.forEach((fileApp) => {
+				fs.rmSync(path.join(appDir, fileApp), { recursive: true, force: true });
+			});
+		});
+	}
+
 	files.forEach((file) => {
-		if (file !== "electron.js") {
-			const filePath = path.join(buildDir, file);
-			fs.rmSync(filePath, { recursive: true, force: true });
-		}
+		fs.cpSync(path.join(buildDir, file), path.join(appDir, file), { recursive: true });
 	});
+
+	const oldPackageJson = packageJson;
+
+	let newPackageJson = {
+		name: "rebound-desktop",
+		version: oldPackageJson.version,
+		private: false,
+		main: "./electron.js",
+		description: "Rebound Nexus official desktop client.",
+		author: oldPackageJson.author,
+		dependencies: oldPackageJson.dependencies,
+	};
+
+	fs.writeFileSync(path.join(appDir, "package.json"), JSON.stringify(newPackageJson));
 });
