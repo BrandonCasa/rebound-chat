23c06177aa52ea02786684c08f37b639d84614a9
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-04-28 10:47:00 -0400

    Add passport-google-oauth20 integration and update passport configuration

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,33 +1,45 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import UserModel from "../models/User.js";
 
 class CustomPassport {
-  setupPassport() {
-    passport.use(
-      new LocalStrategy(
-        {
-          usernameField: "user[email]",
-          passwordField: "user[password]",
-          passReqToCallback: true,
-        },
-        function (req, email, password, done) {
-          UserModel.findOne({ email: email })
-            .then(function (user) {
-              if (!user || !user.validPassword(password)) {
-                return done(null, false, {
-                  errors: { "email or password": "is invalid." },
-                });
-              }
+	setupPassport() {
+		passport.use(
+			new LocalStrategy(
+				{
+					usernameField: "user[email]",
+					passwordField: "user[password]",
+					passReqToCallback: true,
+				},
+				function (req, email, password, done) {
+					UserModel.findOne({ email: email })
+						.then(function (user) {
+							if (!user || !user.validPassword(password)) {
+								return done(null, false, {
+									errors: { "email or password": "is invalid." },
+								});
+							}
 
-              return done(null, user);
-            })
-            .catch(done);
-        },
-      ),
-    );
-  }
+							return done(null, user);
+						})
+						.catch(done);
+				}
+			),
+			new GoogleStrategy(
+				{
+					clientID: GOOGLE_CLIENT_ID,
+					clientSecret: GOOGLE_CLIENT_SECRET,
+					callbackURL: "http://www.example.com/auth/google/callback",
+				},
+				function (accessToken, refreshToken, profile, cb) {
+					User.findOrCreate({ googleId: profile.id }, function (err, user) {
+						return cb(err, user);
+					});
+				}
+			)
+		);
+	}
 }
 
 const customPassport = new CustomPassport();
