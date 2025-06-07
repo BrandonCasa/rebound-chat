2e98a63da8c90633bca02f9863e0815c7d315f02
BrandonCasa <brandoncasa8@gmail.com>
2025-06-07 05:04:15 -0400

    roboto font

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
 import { Provider } from "react-redux";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./index.css";
 
 import store from "./store";
 
