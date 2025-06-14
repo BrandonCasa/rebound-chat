82767107657f33a6f05c8319604c54466d3e4a0a
BrandonCasa <brandoncasa8@gmail.com>
2025-06-14 01:20:44 -0400

    Refactor ServersPage to use ItemBox and enhance ServerCard component

--- src/routes/ServersPage/ServersPage.route.jsx
+++ src/routes/ServersPage/ServersPage.route.jsx
@@ -1,5 +1,8 @@
-import { Box } from "@mui/material";
-import ServerCard from "./ServerCard";
+import { Box, Grid, Paper, Stack, useTheme, Card, CardContent, Typography, CardActionArea, CardMedia } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { TransitionGrid } from "../LandingPage/utils/TransitionGrid";
+import { ItemBox } from "../LandingPage/utils/ItemBox";
+import { scrollbarStyles } from "../LandingPage/utils/scrollbarStyles";
 
 const sampleServers = [
 	{
@@ -42,7 +45,27 @@ const sampleServers = [
 	},
 ];
 
+function ServerCard({ theme, server }) {
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardActionArea>
+				<CardMedia component="img" height="140" alt="among us" />
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						WIP
+					</Typography>
+					<Typography variant="body2" sx={{ color: "text.secondary" }}>
+						this feature isnt done lol
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
+}
+
 function ServersPage() {
+	const theme = useTheme();
+
 	return (
 		<Box
 			sx={{
@@ -52,15 +75,32 @@ function ServersPage() {
 				justifyContent: "center",
 				overflow: "hidden",
 			}}>
-			<Paper
+			<ItemBox
 				sx={{
-					position: "relative",
-					display: "flex",
-					flexDirection: "column",
 					width: "100%",
 					flexGrow: 1,
-					height: "100%",
-				}}></Paper>
+					position: "relative",
+					overflowX: "hidden",
+					overflowY: "auto",
+					...scrollbarStyles,
+				}}>
+				<Box>
+					<Grid container spacing={2} sx={{ justifyContent: "center" }}>
+						<Grid size={{ xs: 12, sm: 6, md: 4 }} alignItems="stretch" display="flex" justifyContent="center">
+							<ServerCard theme={theme} />
+						</Grid>
+						<Grid size={{ xs: 12, sm: 6, md: 4 }} alignItems="stretch" display="flex" justifyContent="center">
+							<ServerCard theme={theme} />
+						</Grid>
+						<Grid size={{ xs: 12, sm: 6, md: 4 }} alignItems="stretch" display="flex" justifyContent="center">
+							<ServerCard theme={theme} />
+						</Grid>
+						<Grid size={{ xs: 12, sm: 6, md: 4 }} alignItems="stretch" display="flex" justifyContent="center">
+							<ServerCard theme={theme} />
+						</Grid>
+					</Grid>
+				</Box>
+			</ItemBox>
 		</Box>
 	);
 }
