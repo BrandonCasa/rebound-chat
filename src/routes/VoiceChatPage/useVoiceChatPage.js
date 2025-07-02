f8259f3b2245b0302dad80da5cc45341fad5d5db
Brandon <brandoncasa8@gmail.com>
2025-07-02 00:12:32 -0400

    Add VoiceChat page skeleton

---
+++ src/routes/VoiceChatPage/useVoiceChatPage.js
@@ -0,0 +1,5 @@
+export default function useVoiceChatPage() {
+    // Placeholder hook for VoiceChatPage state management.
+    // Actual logic will be handled via Redux in the future.
+    return {};
+}
