e2012aac515085403cb96492012b8fde3428fbfa
Brandon <brandoncasa8@gmail.com>
2025-11-28 09:20:55 -0500

    Update socketSlice.js

--- src/slices/socketSlice.js
+++ src/slices/socketSlice.js
@@ -6,7 +6,7 @@ let lifecycleHandlers = null;
 const resolveDefaultSocketURL = () => {
 	if (process.env.NODE_ENV === "development") return "http://localhost:6002";
 	if (globalThis.IN_ELECTRON_ENV) return "https://rebound.nexus";
-	if (typeof window !== "undefined" && window.location?.origin) return window.location.origin;
+	//if (typeof window !== "undefined" && window.location?.origin) return window.location.origin;
 	return "";
 };
 
